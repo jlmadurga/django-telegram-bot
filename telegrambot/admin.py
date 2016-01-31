46b7927a01e96d5f2ef4e75c996701abe8f7894c
Juan Madurga <jlmadurga@gmail.com>
2016-01-31 15:20:53 +0100

    multiple bots

--- telegrambot/admin.py
+++ telegrambot/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from telegrambot.models import Message, Chat, Update, User
+from telegrambot.models import Message, Chat, Update, User, Bot
 
 admin.site.register(Message)
 admin.site.register(Chat)
 admin.site.register(User)
 admin.site.register(Update)
+admin.site.register(Bot)
---
+++ telegrambot/models/__init__.py
@@ -0,0 +1,2 @@
+from telegram_api import (User, Chat, Message, Update)  # NOQA
+from bot import Bot  # NOQA
