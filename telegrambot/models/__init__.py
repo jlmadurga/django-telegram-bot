b4b907cbb54ec89a4edd9d69bfe9705046ae4c13
Juan Madurga <jlmadurga@gmail.com>
2016-01-31 15:23:57 +0100

    full path imports for python3

--- telegrambot/models/__init__.py
+++ telegrambot/models/__init__.py
@@ -1,2 +1,2 @@
-from telegram_api import (User, Chat, Message, Update)  # NOQA
-from bot import Bot  # NOQA
+from telegrambot.models.telegram_api import (User, Chat, Message, Update)  # NOQA
+from telegrambot.models.bot import Bot  # NOQA
