72ba6218bda68d248b0e665b67ef95bad27c1479
Juan Madurga <jlmadurga@gmail.com>
2016-06-01 15:58:17 +0200

    Bump version: 0.5.2 → 0.5.3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='django-telegram-bot',
-    version="0.5.2",
+    version="0.5.3",
     description="""Django app to write Telegram bots""",
     long_description=readme + '\n\n' + history,
     author='Juan Madurga',
