b470d1d20184ce0d69e8cbbd99a9a87f610f14ec
muratjumashev <jumasheff@gmail.com>
2019-03-31 22:20:39 +0600

    Save update if only it's required in settings

--- telegrambot/views.py
+++ telegrambot/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from telegram import Update
 import logging
+from django.conf import settings
 from django.views import generic
 import sys
 import traceback
@@ -12,28 +13,34 @@ import traceback
 logger = logging.getLogger(__name__)
 
 class WebhookView(APIView):
-    
+    def _post(self, request, token):
+        try:
+            bot = Bot.objects.get(token=token)
+            bot.handle(Update.de_json(request.data, bot._bot))
+        except Bot.DoesNotExist:
+            logger.warning("Token %s not associated to a bot" % token)
+            return Response({"error": "token is not there"}, status=status.HTTP_404_NOT_FOUND)
+        except:
+            exc_info = sys.exc_info()
+            traceback.print_exception(*exc_info)
+            logger.error("Error processing %s for token %s" % (request.data, token))
+            return Response({"error": "something went wrong"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response({"message": "ok"}, status=status.HTTP_200_OK)
+        return Response({"error": "bad request"}, status=status.HTTP_400_BAD_REQUEST)
+
     def post(self, request, token):
-        serializer = UpdateSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            try:
-                bot = Bot.objects.get(token=token)
-                bot.handle(Update.de_json(request.data, bot._bot))
-            except Bot.DoesNotExist:
-                logger.warning("Token %s not associated to a bot" % token)
-                return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-            except:
-                exc_info = sys.exc_info()
-                traceback.print_exception(*exc_info)
-                logger.error("Error processing %s for token %s" % (request.data, token))
-                return Response(serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        if settings.SAVE_UPDATES:
+            serializer = UpdateSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return self._post(request, token)
             else:
-                return Response(serializer.data, status=status.HTTP_200_OK)
-        logger.error("Validation error: %s from message %s" % (serializer.errors, request.data))
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    
+                logger.error("Validation error: %s from message %s" % (serializer.errors, request.data))
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self._post(request, token)
+
+
 class AuthView(generic.TemplateView):
     template_name = 'telegrambot/authentication.html'
     
